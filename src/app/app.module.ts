defe785df75147176aaa5aa93fabdb716fea9285
seungboklee <lsb530@naver.com>
2022-07-19 18:23:28 +0900

    11강까지 완료/ 12강 진행중

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SectionComponent } from './section/section.component';
+import { FooterComponent } from './footer/footer.component';
+import { TimeDisplayComponent } from './section/time-display/time-display.component';
+import { ButtonsComponent } from './section/buttons/buttons.component';
+import {LayoutModule} from "./layout/layout.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SectionComponent,
+    FooterComponent,
+    TimeDisplayComponent,
+    ButtonsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule
   ],
   providers: [],
+  exports: [
+    AppComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/section/time-display/time-display.component.ts
@@ -0,0 +1,58 @@
+import {Component, Input, OnInit, SimpleChanges} from '@angular/core';
+
+@Component({
+  selector: 'app-time-display',
+  templateUrl: './time-display.component.html',
+  styleUrls: ['./time-display.component.scss']
+})
+export class TimeDisplayComponent implements OnInit {
+
+  @Input() inputData: string = '';
+
+  min: number = 0
+  sec: number = 0
+  ms: number = 0
+  timeInterval: any;
+
+  constructor() {
+    console.log(this.inputData);
+  }
+
+  timeStart() {
+    this.timeInterval = setInterval(() => {
+      this.ms++;
+    }, 10);
+  }
+
+  timeStop() {
+    clearTimeout(this.timeInterval);
+  }
+
+  timeReset() {
+    this.timeStop();
+    this.ms = 0;
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes);
+    for (let propName in changes) {
+      if(propName === 'inputData') {
+        switch (changes[propName].currentValue) {
+          case 'start':
+            this.timeStart();
+            break;
+          case 'stop':
+            this.timeStop();
+            break;
+          case 'reset':
+            this.timeReset();
+            break;
+        }
+      }
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+}
